dedbab7938c88f2a13b6031ecaf26463305245d5
Gautam Krishnan <gautamkrishnan7209@gmail.com>
2018-07-19 01:49:34 +0530

    1.Wired up Recycler View. 2.Added a FAB.

---
+++ app/src/main/java/com/techieaid/howler/AlarmRecyclerViewAdapter.java
@@ -0,0 +1,54 @@
+package com.techieaid.howler;
+
+import android.content.Context;
+import android.media.Image;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ ** Created by Gautam Krishnan {@link https://github.com/GautiKrish}
+ */public class AlarmRecyclerViewAdapter extends RecyclerView.Adapter<AlarmRecyclerViewAdapter.ViewHolder> {
+
+     private Context mContext;
+     private ArrayList<String> mAlarmsAdapter;
+
+    AlarmRecyclerViewAdapter(Context context, ArrayList<String> alarmsAdapter) {
+        this.mContext = context;
+        mAlarmsAdapter=alarmsAdapter;
+    }
+    @NonNull
+    @Override
+    public AlarmRecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.activity_alarm_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AlarmRecyclerViewAdapter.ViewHolder holder, int position) {
+        holder.mTextView.setText(mAlarmsAdapter.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView mTextView;
+        private ImageView mImageView;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            mTextView = itemView.findViewById(R.id.alarm_time);
+            mImageView = itemView.findViewById(R.id.trash_icon);
+        }
+    }
+}
--- app/src/main/java/com/techieaid/howler/MainActivity.java
+++ app/src/main/java/com/techieaid/howler/MainActivity.java
@@ -2,10 +2,17 @@ package com.techieaid.howler;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    RecyclerView mRecyclerView;
+    AlarmRecyclerViewAdapter mAdapter;
+    private ArrayList<String> alarms = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +20,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setSupportActionBar(findViewById(R.id.custom_toolbar));
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+        alarms = new ArrayList<>();
+        alarms.add("04:30");
+        alarms.add("05:00");
+        alarms.add("05:30");
+        alarms.add("06:00");
+        alarms.add("19:00");
+        alarms.add("08:30");
+        alarms.add("22:00");
+        alarms.add("00:00");
+        alarms.add("11:40");
+        alarms.add("01:00");
+        mRecyclerView = findViewById(R.id.alarm_item);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mAdapter = new AlarmRecyclerViewAdapter(this,alarms);
+        mRecyclerView.setAdapter(mAdapter);
 
 
     }
