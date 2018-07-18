33f6b2873b19954f1526ebbc786ca8f3539c9bbd
Gautam Krishnan <gautamkrishnan7209@gmail.com>
2018-07-18 19:05:21 +0530

    1.Changed the app icon. 2.Added a splash screen. 3.Added a custom toolbar.

---
+++ app/src/main/java/com/techieaid/howler/MainActivity.java
@@ -0,0 +1,19 @@
+package com.techieaid.howler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setSupportActionBar(findViewById(R.id.custom_toolbar));
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+
+    }
+}
