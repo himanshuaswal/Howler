013f2b4633cf6ebc22dca56f750741bf69225709
Gautam Krishnan <gautamkrishnan7209@gmail.com>
2018-07-20 07:34:40 +0530

    Added animation to question text view.

--- app/src/main/java/com/techieaid/howler/SnoozeActivity.java
+++ app/src/main/java/com/techieaid/howler/SnoozeActivity.java
@@ -2,12 +2,30 @@ package com.techieaid.howler;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class SnoozeActivity extends AppCompatActivity {
+    private TextView mQuestionTextView;
+    private Animation animation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_snooze);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN |
+                        WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
+                        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+                        WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN |
+                        WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
+                        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+                        WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+        mQuestionTextView = findViewById(R.id.question);
+        animation = AnimationUtils.loadAnimation(this,R.anim.fade_in);
+        mQuestionTextView.startAnimation(animation);
+
     }
 }
